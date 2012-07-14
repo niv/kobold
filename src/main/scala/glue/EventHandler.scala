fa6aee2d2a5d0892548a2a069f8b434aac814af2
niv <n@e-ix.net>
2012-07-14 19:49:40 +0200

    print time spent in VM/out of VM

--- src/main/scala/glue/EventHandler.scala
+++ src/main/scala/glue/EventHandler.scala
@@ -14,6 +14,10 @@ private [glue] object EventHandler extends SchedulerListener with Logging {
   /** The namespace for game->kobold management calls. */
   val InternalRegexp = """^kobold\.(.+)$""".r
 
+  val TimeLogInterval = 10000
+
+  private var nsThisInterval: Long = 0
+
   def postFlushQueues(remainingTokens: Int) {}
   def missedToken(objSelf: NWObject, token: String) {}
   def context(objSelf: NWObject) {}
@@ -43,41 +47,61 @@ private [glue] object EventHandler extends SchedulerListener with Logging {
     NWScript.setLocalString(Module(), "_" + fun, if (ret) "1" else "0")
   }
 
-  def event(objSelf: NWObject, event: String) {
-    case class E(e: String, o: NWObject)
+  def event(objSelf: NWObject, event: String) = {
+    val start = System.nanoTime
+    try {
+      case class E(e: String, o: NWObject)
 
-    def e[X <: IObject](self: X)(event: String, va: Object*) =
-      Host.handleObjectEvent(event, va.toList)(self)
+      def e[X <: IObject](self: X)(event: String, va: Object*) =
+        Host.handleObjectEvent(event, va.toList)(self)
 
-    val ctx: Set[Context[_]] = E(event, objSelf) match {
-      case E(InternalRegexp(a), o: IObject) =>
-        kobold(a, o)
-        Set()
+      val ctx: Set[Context[_]] = E(event, objSelf) match {
+        case E(InternalRegexp(a), o: IObject) =>
+          kobold(a, o)
+          Set()
 
-      case E("creature_spawn", o: ICreature) =>
-        e(o)("creature.spawn")
+        case E("creature_spawn", o: ICreature) =>
+          e(o)("creature.spawn")
 
-      case E("creature_perc", o: ICreature) =>
-        val (heard, seen, inaudible, vanished, last) = (
-          NWScript.getLastPerceptionHeard, NWScript.getLastPerceptionSeen,
-          NWScript.getLastPerceptionInaudible, NWScript.getLastPerceptionVanished,
-          NWScript.getLastPerceived
-        )
-        require(List(heard, seen, inaudible, vanished).count(_ == true) == 1)
-        if (heard) e(o)("creature.hears", last)
-        else if (seen) e(o)("creature.sees", last)
-        else if (vanished) e(o)("creature.seesnot", last)
-        else if (inaudible) e(o)("creature.hearsnot", last)
-        else Set()
+        case E("creature_perc", o: ICreature) =>
+          val (heard, seen, inaudible, vanished, last) = (
+            NWScript.getLastPerceptionHeard, NWScript.getLastPerceptionSeen,
+            NWScript.getLastPerceptionInaudible, NWScript.getLastPerceptionVanished,
+            NWScript.getLastPerceived
+          )
+          require(List(heard, seen, inaudible, vanished).count(_ == true) == 1)
+          if (heard) e(o)("creature.hears", last)
+          else if (seen) e(o)("creature.sees", last)
+          else if (vanished) e(o)("creature.seesnot", last)
+          else if (inaudible) e(o)("creature.hearsnot", last)
+          else Set()
 
-      case E("creature_hb", o: ICreature) =>
-        e(o)("creature.hb") // Host.onCreatureHB(o) // for task manager!
+        case E("creature_hb", o: ICreature) =>
+          e(o)("creature.hb") // Host.onCreatureHB(o) // for task manager!
 
-      case e =>
-        log.debug("unhandled: " + e)
-        Set()
-    }
+        case e =>
+          log.debug("unhandled: " + e)
+          Set()
+      }
 
-    Scheduler.flushQueues
+      Scheduler.flushQueues
+    } catch {
+      case any =>
+        log.error(any, "event = " + event + " on = " + objSelf)
+
+    } finally
+      nsThisInterval += System.nanoTime - start
   }
+
+  new java.util.Timer().scheduleAtFixedRate(new java.util.TimerTask {
+    def run = if (nsThisInterval != 0) {
+      val msIn = nsThisInterval / 1000 / 1000
+      val msOut = TimeLogInterval - msIn
+      log.debug("Time spent in VM: %dms, in nwserver: %dms => %d%%".format(
+        msIn, msOut, msIn / msOut * 100
+      ))
+      nsThisInterval = 0
+    }
+  }, 0, TimeLogInterval)
+
 }
